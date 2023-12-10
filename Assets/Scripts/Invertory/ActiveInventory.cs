33362a23a601d0f7112dd811a747d0b7e63b6d1b
caner <canerdk1995@hotmail.com>
2023-12-10 21:24:28 +0300

    edited bow and staff animation

--- Assets/Scripts/Invertory/ActiveInventory.cs
+++ Assets/Scripts/Invertory/ActiveInventory.cs
@@ -47,15 +47,16 @@ public class ActiveInventory : MonoBehaviour
             Destroy(ActiveWeapon.Instance.CurrentActiveWeapon.gameObject);
         }
 
-        if(!transform.GetChild(activeSlotIndexNum).GetComponentInChildren<InventorySlot>()){
+        if (!transform.GetChild(activeSlotIndexNum).GetComponentInChildren<InventorySlot>())
+        {
             ActiveWeapon.Instance.WeaponNull();
             return;
         }
-        
+
         GameObject weaponToSpawn = transform.GetChild(activeSlotIndexNum).GetComponentInChildren<InventorySlot>().GetWeaponInfo().weaponPrefab;
 
         GameObject newWeapon = Instantiate(weaponToSpawn, ActiveWeapon.Instance.transform.position, Quaternion.identity);
-
+        ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, 0);
         newWeapon.transform.parent = ActiveWeapon.Instance.transform;
 
         ActiveWeapon.Instance.NewWeapon(newWeapon.GetComponent<MonoBehaviour>());
